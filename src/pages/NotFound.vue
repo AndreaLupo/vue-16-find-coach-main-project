3f7d99573840574c991920dae379704f32c47d41
Wolf Developer <lupo.andrea.93@gmail.com>
2022-11-26 19:39:15 +0100

    Not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,5 +1,10 @@
 <template>
-  Not found
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>This page could not be found. Maybe check out all our <router-link to="/coaches">coaches</router-link></p>
+    </base-card>
+  </section>
 </template>
 
 <script>

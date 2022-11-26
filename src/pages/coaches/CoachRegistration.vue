c6deea3eed5922735c045d7c682167f30d58f581
Wolf Developer <lupo.andrea.93@gmail.com>
2022-11-26 15:32:43 +0100

    Add form

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,10 +1,19 @@
 <template>
-  Registration
+  <base-card>
+    <section>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </section>
+  </base-card>
 </template>
 
 <script>
-export default {
+import CoachForm from '../../components/coaches/CoachForm.vue';
 
+export default {
+  components: {
+    CoachForm
+  }
 }
 </script>
 

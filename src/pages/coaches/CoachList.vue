2155a7df6fa6b50874be1278d8e136f1ab7c69e8
Wolf Developer <lupo.andrea.93@gmail.com>
2022-11-26 18:46:04 +0100

    Error management with dialog

--- src/pages/coaches/CoachList.vue
+++ src/pages/coaches/CoachList.vue
@@ -1,4 +1,8 @@
 <template>
+    <!-- !!error convert a truthy string (like 'true') in a real boolean value -->
+    <base-dialog :show="!!error" title="An error occured" @close="handleError">
+        <p>{{ error }}</p>
+    </base-dialog>
     <section>
         <coach-filter @change-filter="setFilters"></coach-filter>
     </section>
@@ -36,6 +40,7 @@ export default {
     data() {
         return {
             isLoading: false,
+            error: null,
             activeFilters: {
                 frontend: true,
                 backend: true,
@@ -75,8 +80,15 @@ export default {
         },
         async loadCoaches() {
             this.isLoading = true;
-            await this.$store.dispatch('coaches/loadCoaches');
+            try {
+                await this.$store.dispatch('coaches/loadCoaches');
+            } catch (error) {
+                this.error = error.message || 'Something went wrong!';
+            }
             this.isLoading = false;
+        },
+        handleError() {
+            this.error = null;
         }
     }
 }

442e0f37a83044f105377d4857dd542f1553c261
Wolf Developer <lupo.andrea.93@gmail.com>
2022-11-27 10:23:22 +0100

    Protecting apis with auth

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -23,7 +23,9 @@ export default {
     },
     async fetchRequests(context) {
         const coachId = context.rootGetters.userId;
-        const response = await fetch(`https://vue-http-demo-2c8cb-default-rtdb.firebaseio.com/requests/${coachId}.json`);
+        const token = context.rootGetters.token;
+
+        const response = await fetch(`https://vue-http-demo-2c8cb-default-rtdb.firebaseio.com/requests/${coachId}.json?auth=${token}`);
     
         const responseData = await response.json();
         
